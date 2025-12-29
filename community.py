26193d092a6817e97c905c6276663f1e2f5abe9f
Eduardo de Souza <eduardo@organizemeucondominio.com.br>
2025-12-29 09:19:56 -0300

    29/12 - community ingest

---
+++ community.py
@@ -0,0 +1,8 @@
+
+from src.ingest import community_ingest
+
+if __name__ == "__main__":
+    
+    print("--- Iniciando teste ---\n")
+    community_ingest()
+    print("\n--- Teste finalizado ---")
--- src/ingest/__init__.py
+++ src/ingest/__init__.py
@@ -1,3 +1,4 @@
-from .graphdb_insert  import graph_ingest
+from .graphdb_insert    import graph_ingest
+from .graphdb_community import community_ingest
 
-__all__ = ['graph_ingest']
+__all__ = ['graph_ingest','community_ingest']
